4a1ac11f467f8d7156dbeb45644fccbfb0436c45
unknown <mohit1521sancheti@gmail.com>
2024-08-16 22:19:55 +0530

    established connection to mongodb

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+})
+
+/*
+import express, { application } from "express";
+const app = express()
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on('error', (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on PORT ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error("ERROR", error)
+        throw err 
+    }
+})()
+*/
+
+connectDB()
